b2490b13ad00a5d4855398e65357cdfca993f6bc
Angel Mendez <angelmendezcampal@gmail.com>
2025-08-17 00:07:32 +0200

    fix(react): exclude tests from tsc and remove jest-dom from compiler types

--- packages/react/src/tests/setupTests.ts
+++ packages/react/src/tests/setupTests.ts
@@ -2,10 +2,9 @@ import '@testing-library/jest-dom/vitest';
 import { afterEach, beforeEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 /// <reference types="@testing-library/jest-dom" />
+import '@testing-library/jest-dom';
 import matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
-import '@testing-library/jest-dom';
-
 expect.extend(matchers);
 
 beforeEach(() => {
